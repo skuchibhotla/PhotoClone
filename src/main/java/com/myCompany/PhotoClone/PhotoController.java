d9b369ceb2b183b052de4b7c318b114441e474d7
Shashank Kuchibhotla <sk.kuchibhotla@gmail.com>
2024-03-30 15:12:37 -0600

    Initial commit

---
+++ src/main/java/com/myCompany/PhotoClone/PhotoController.java
@@ -0,0 +1,47 @@
+package com.myCompany.PhotoClone;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
+
+@RestController
+public class PhotoController {
+
+    private Map<String, Photo> db = new HashMap<>() {{
+       put("1", new Photo("1", "hello.jpg"));
+    }};
+
+//    private List<Photo> db = List.of(new Photo("1", "hello.jpg"));
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/photo")
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photo/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photo/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photo")
+    public void create(@RequestBody @Valid Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+    }
+}
---
+++ src/test/java/com/myCompany/PhotoClone/PhotoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCompany.PhotoClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
